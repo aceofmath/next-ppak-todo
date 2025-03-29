4e62a3d6791771d50eba74b7c4b4526397fce60d
aceofmath <aceofmath@naver.com>
2025-03-29 23:58:35 +0900

    3:33:19 모달 띄우기 전까지

--- components/todos-table.tsx
+++ components/todos-table.tsx
@@ -7,97 +7,139 @@ import { Popover, PopoverTrigger, PopoverContent } from "@heroui/popover";
 import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@heroui/table";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { Spinner } from "@heroui/spinner";
+import { ToastContainer, toast } from "react-toastify";
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@heroui/dropdown";
+import { VerticalDotsIcon } from "./icons";
 
 export default function TodosTables({ todos }: { todos: Todo[] }) {
-    //할일 추가 가능 여부
-    const [todoAddEnable, setTodoAddEnable] = useState(false);
+  //할일 추가 가능 여부
+  const [todoAddEnable, setTodoAddEnable] = useState(false);
 
-    //입력된 할일
-    const [newTodoInput, setNewTodoInput] = useState("");
+  //입력된 할일
+  const [newTodoInput, setNewTodoInput] = useState("");
 
-    const router = useRouter();
+  //로딩상태
+  const [isLoading, setIsLoading] = useState(false);
 
-    const TodoRow = (aTodo: Todo) => {
-        return (
-            <TableRow key={aTodo.id}>
-                <TableCell>{aTodo.id.slice(0, 4)}</TableCell>
-                <TableCell>{aTodo.title}</TableCell>
-                <TableCell>{aTodo.is_done ? "✅" : "❓"}</TableCell>
-                <TableCell>{`${aTodo.created_at}`}</TableCell>
-            </TableRow>
-        );
-    };
+  const router = useRouter();
 
-    const addATodoHandler = async (title: string) => {
-        console.log("title", title);
-        if (!todoAddEnable) {
-            return;
-        }
-
-        console.log("addATodoHandler called");
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos`, {
-            method: "POST",
-            body: JSON.stringify({
-                title: title,
-            }),
-            cache: "no-store",
-        });
-        router.refresh();
-        console.log(`할일 추가 완료 : ${newTodoInput}`);
-    };
-
-    const DisabledTodoAddButton = () => {
-        return (
-            <Popover placement="top">
-                <PopoverTrigger>
-                    <Button color="default" className="h-14">
-                        추가
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                    <div className="px-1 py-2">
-                        <div className="text-small font-bold">🦉</div>
-                        <div className="text-tiny">할일을 입력해주세요!</div>
-                    </div>
-                </PopoverContent>
-            </Popover>
-        );
-    };
+  const TodoRow = (aTodo: Todo) => {
     return (
-        <>
-            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                <Input
-                    label="새로운 할일"
-                    type="text"
-                    value={newTodoInput}
-                    onValueChange={(changedInput) => {
-                        setNewTodoInput(changedInput);
-                        setTodoAddEnable(changedInput.length > 0);
-                    }}
-                />
-                {todoAddEnable ? (
-                    <Button
-                        color="warning"
-                        className="h-14"
-                        onPress={async () => {
-                            await addATodoHandler(newTodoInput);
-                        }}
-                    >
-                        추가
-                    </Button>
-                ) : (
-                    DisabledTodoAddButton()
-                )}
-            </div>
-            <Table aria-label="Example static collection table">
-                <TableHeader>
-                    <TableColumn>아이디</TableColumn>
-                    <TableColumn>할일내용</TableColumn>
-                    <TableColumn>완료여부</TableColumn>
-                    <TableColumn>생성일</TableColumn>
-                </TableHeader>
-                <TableBody emptyContent={"조회된 데이터가 없습니다."}>{todos && todos.map((aTodo: Todo) => TodoRow(aTodo))}</TableBody>
-            </Table>
-        </>
+      <TableRow key={aTodo.id}>
+        <TableCell>{aTodo.id.slice(0, 4)}</TableCell>
+        <TableCell>{aTodo.title}</TableCell>
+        <TableCell>{aTodo.is_done ? "✅" : "❓"}</TableCell>
+        <TableCell>{`${aTodo.created_at}`}</TableCell>
+        <TableCell>
+          <div className="relative flex justify-end items-center gap-2">
+            <Dropdown>
+              <DropdownTrigger>
+                <Button isIconOnly size="sm" variant="light">
+                  <VerticalDotsIcon className="text-default-300" />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu>
+                <DropdownItem key="view">상세보기</DropdownItem>
+                <DropdownItem key="edit">수정</DropdownItem>
+                <DropdownItem key="delete">삭제</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        </TableCell>
+      </TableRow>
     );
+  };
+
+  const addATodoHandler = async (title: string) => {
+    console.log("title", title);
+    if (!todoAddEnable) {
+      return;
+    }
+
+    setTodoAddEnable(false);
+    setIsLoading(true);
+    // setTimeout(() => {
+    //   console.log("....");
+    // }, 5000);
+    await new Promise((f) => setTimeout(f, 600));
+
+    console.log("addATodoHandler called");
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos`, {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+      }),
+      cache: "no-store",
+    });
+
+    setNewTodoInput("");
+    router.refresh();
+    setIsLoading(false);
+    notifyTodoAddedEvent("할일 추가 되었습니다.");
+    console.log(`할일 추가 완료 : ${newTodoInput}`);
+  };
+
+  const DisabledTodoAddButton = () => {
+    return (
+      <Popover placement="top">
+        <PopoverTrigger>
+          <Button color="default" className="h-14">
+            추가
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="px-1 py-2">
+            <div className="text-small font-bold">🦉</div>
+            <div className="text-tiny">할일을 입력해주세요!</div>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
+  };
+  const notifyTodoAddedEvent = (msg: string) => toast.success(msg);
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <div>
+        <ToastContainer position="top-right" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick={false} rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
+      </div>
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <Input
+          label="새로운 할일"
+          type="text"
+          value={newTodoInput}
+          onValueChange={(changedInput) => {
+            setNewTodoInput(changedInput);
+            setTodoAddEnable(changedInput.length > 0);
+          }}
+        />
+        {todoAddEnable ? (
+          <Button
+            color="warning"
+            className="h-14"
+            onPress={async () => {
+              await addATodoHandler(newTodoInput);
+            }}
+          >
+            추가
+          </Button>
+        ) : (
+          DisabledTodoAddButton()
+        )}
+      </div>
+      <div className="h-2">{isLoading && <Spinner size="sm" color="warning" />}</div>
+
+      <Table aria-label="Example static collection table">
+        <TableHeader>
+          <TableColumn>아이디</TableColumn>
+          <TableColumn>할일내용</TableColumn>
+          <TableColumn>완료여부</TableColumn>
+          <TableColumn>생성일</TableColumn>
+          <TableColumn>기능</TableColumn>
+        </TableHeader>
+        <TableBody emptyContent={"조회된 데이터가 없습니다."}>{todos && todos.map((aTodo: Todo) => TodoRow(aTodo))}</TableBody>
+      </Table>
+    </div>
+  );
 }
--- data/firestore.js
+++ data/firestore.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import {getFirestore, collection, getDocs, getDoc, setDoc, deleteDoc, updateDoc, doc, Timestamp} from 'firebase/firestore';
+import { getFirestore, collection, getDocs, getDoc, setDoc, deleteDoc, updateDoc, doc, Timestamp, query, orderBy } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -11,7 +11,7 @@ const firebaseConfig = {
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
@@ -20,112 +20,113 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 //LIST
-export async function fetchTodos(){
-    const querySnapshot = await getDocs(collection(db, "todos"));
+export async function fetchTodos() {
+  const todosRef = collection(db, "todos");
+  const q = query(todosRef, orderBy("created_at", "desc"));
+  //   const querySnapshot = await getDocs(collection(db, "todos"));
+  const querySnapshot = await getDocs(q);
 
-    if (querySnapshot.empty) {
-        return [];
-    }
+  if (querySnapshot.empty) {
+    return [];
+  }
 
-    const fecchedTodos = [];
+  const fecchedTodos = [];
 
-    querySnapshot.forEach((doc) => {
-        console.log(doc.id, "===>", doc.data());
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, "===>", doc.data());
 
-        const aTodo = {
-            id:doc.id,
-            title:doc.data()["title"],
-            is_done:doc.data()["is_done"],
-            created_at:doc.data()["created_at"].toDate(),
-        }
-        fecchedTodos.push(aTodo);
-    });
+    const aTodo = {
+      id: doc.id,
+      title: doc.data()["title"],
+      is_done: doc.data()["is_done"],
+      created_at: doc.data()["created_at"].toDate(),
+    };
+    fecchedTodos.push(aTodo);
+  });
 
-    return fecchedTodos;
+  return fecchedTodos;
 }
 
 //INSERT
-export async function addATodo({title}){
-    const newTodoRef = doc(collection(db, "todos"));
+export async function addATodo({ title }) {
+  const newTodoRef = doc(collection(db, "todos"));
 
-    const createdAtTimestamp = Timestamp.fromDate(new Date());
+  const createdAtTimestamp = Timestamp.fromDate(new Date());
 
-    const newTodoData = {
-        id : newTodoRef.id,
-        title,
-        is_done:false,
-        created_at:createdAtTimestamp
-    }
+  const newTodoData = {
+    id: newTodoRef.id,
+    title,
+    is_done: false,
+    created_at: createdAtTimestamp,
+  };
 
-    await setDoc(newTodoRef, newTodoData);
+  await setDoc(newTodoRef, newTodoData);
 
-    return {
-        id : newTodoRef.id,
-        title:title,
-        is_done:false,
-        created_at:createdAtTimestamp.toDate()
-    };
+  return {
+    id: newTodoRef.id,
+    title: title,
+    is_done: false,
+    created_at: createdAtTimestamp.toDate(),
+  };
 }
 
 //GET
-export async function fetchATodo(id){
-    if(id === null){
-        return null;
-    }
-    const newDocRef = doc(db, "todos", id);
+export async function fetchATodo(id) {
+  if (id === null) {
+    return null;
+  }
+  const newDocRef = doc(db, "todos", id);
 
-    const todoDocSnap = await getDoc(newDocRef);
+  const todoDocSnap = await getDoc(newDocRef);
 
-    if (todoDocSnap.exists()){
-        console.log("Document data:", todoDocSnap.data());
+  if (todoDocSnap.exists()) {
+    console.log("Document data:", todoDocSnap.data());
 
-        const fetchednTodo = {
-            id : todoDocSnap.id,
-            title : todoDocSnap.data()["title"],
-            is_done:todoDocSnap.data()["is_done"],
-            created_at:todoDocSnap.data()["created_at"].toDate(),
-        }
+    const fetchednTodo = {
+      id: todoDocSnap.id,
+      title: todoDocSnap.data()["title"],
+      is_done: todoDocSnap.data()["is_done"],
+      created_at: todoDocSnap.data()["created_at"].toDate(),
+    };
 
-        return fetchednTodo;
-
-    }else{
-        console.log("no data")
-        return null;
-
-    }   
+    return fetchednTodo;
+  } else {
+    console.log("no data");
+    return null;
+  }
 }
 
 //DELETE
-export async function deleteATodo(id){
-    const fetchedTodo = await fetchATodo(id);
+export async function deleteATodo(id) {
+  const fetchedTodo = await fetchATodo(id);
 
-    if(fetchedTodo === null){
-        return null;
-    }
-  
-    await deleteDoc(doc(db, "todos", id));
-    
-    return fetchedTodo;
+  if (fetchedTodo === null) {
+    return null;
+  }
+
+  await deleteDoc(doc(db, "todos", id));
+
+  return fetchedTodo;
 }
 
 //UPDATE
-export async function editATodo(id, {title, is_done}){
-    const fetchedTodo = await fetchATodo(id);
+export async function editATodo(id, { title, is_done }) {
+  const fetchedTodo = await fetchATodo(id);
 
-    if(fetchedTodo === null){
-        return null;
-    }
+  if (fetchedTodo === null) {
+    return null;
+  }
 
-    const todoRef = doc(db, "todos", id);
-  
-    const updatedTodo = await updateDoc(todoRef, {title, is_done});
-    
-    return {
-        id,
-        title,
-        is_done,
-        created_at:fetchedTodo.created_at,
-    };
+  const todoRef = doc(db, "todos", id);
+
+  const updatedTodo = await updateDoc(todoRef, { title, is_done });
+
+  return {
+    id,
+    title,
+    is_done,
+    created_at: fetchedTodo.created_at,
+  };
 }
 
-module.exports = {fetchTodos, addATodo, fetchATodo, deleteATodo, editATodo};
+module.exports = { fetchTodos, addATodo, fetchATodo, deleteATodo, editATodo };
