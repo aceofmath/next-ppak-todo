4dc9890318994ceaf277a58a9c0efdf13a686ac0
aceofmath <aceofmath@naver.com>
2025-04-01 13:12:46 +0900

    예외처리2

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,10 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import dummytodos from "@/data/dummy.json";
-import {fetchTodos, addATodo} from '@/data/firestore'
+import { fetchTodos, addATodo } from "@/data/firestore";
 
 //LIST
 export async function GET(request: NextRequest) {
     const fetchedTodos = await fetchTodos();
+
+    if (fetchedTodos === null) {
+        return new Response(null, { status: 204 });
+    }
 
     const response = {
         message: "todos 몽땅 가져오기",
@@ -17,14 +21,14 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
     const { title } = await request.json();
 
-    if(title === undefined){
+    if (title === undefined) {
         const errMessage = {
-            message : '할일을 작성해주세요.'
-        }
+            message: "할일을 작성해주세요.",
+        };
         return NextResponse.json(errMessage, { status: 422 });
     }
 
-    const addedTodo = await addATodo({title});   
+    const addedTodo = await addATodo({ title });
 
     const response = {
         message: "할일 추가 성공!",
