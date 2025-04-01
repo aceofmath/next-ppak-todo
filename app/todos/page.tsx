2d2fbf69fa7d83e7ed838a664ed00148692613a6
aceofmath <aceofmath@naver.com>
2025-04-01 12:48:53 +0900

    에러 처리

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -5,15 +5,24 @@ import { fetchTodos } from "@/data/firestore";
 async function fetchTodosApiCall() {
     console.log("fetchTodosApiCall called");
     const res = await fetch(`${process.env.BASE_URL}/api/todos`);
+    const contentTypeHeaderValue = res.headers.get("Content-Type");
+    //text/html;
+    if (contentTypeHeaderValue?.includes("text/html")) {
+        console.log("fetchTodosApiCall / contentTypeHeaderValue : ", contentTypeHeaderValue);
+        return null;
+    }
+
     return res.json();
 }
 
 export default async function TodosPage() {
     const response = await fetchTodosApiCall();
+
+    const fechedTodos = response?.data ?? [];
     return (
         <div className="flex flex-col space-y-8">
             <h1 className={title()}>Todos</h1>
-            <TodosTables todos={response.data ?? []} />
+            <TodosTables todos={fechedTodos} />
         </div>
     );
 }
