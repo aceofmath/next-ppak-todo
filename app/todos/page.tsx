b8bc8450f8c59f450d4a1ac73c53403f91e1153f
aceofmath <aceofmath@naver.com>
2025-04-09 14:58:47 +0900

    목록 가져오기 no cache설정

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -3,26 +3,28 @@ import TodosTables from "@/components/todos-table";
 import { fetchTodos } from "@/data/firestore";
 
 async function fetchTodosApiCall() {
-    console.log("fetchTodosApiCall called");
-    const res = await fetch(`${process.env.BASE_URL}/api/todos`);
-    const contentTypeHeaderValue = res.headers.get("Content-Type");
-    //text/html;
-    if (contentTypeHeaderValue?.includes("text/html")) {
-        console.log("fetchTodosApiCall / contentTypeHeaderValue : ", contentTypeHeaderValue);
-        return null;
-    }
+  console.log("fetchTodosApiCall called");
+  const res = await fetch(`${process.env.BASE_URL}/api/todos`, {
+    cache: "no-cache",
+  });
+  const contentTypeHeaderValue = res.headers.get("Content-Type");
+  //text/html;
+  if (contentTypeHeaderValue?.includes("text/html")) {
+    console.log("fetchTodosApiCall / contentTypeHeaderValue : ", contentTypeHeaderValue);
+    return null;
+  }
 
-    return res.json();
+  return res.json();
 }
 
 export default async function TodosPage() {
-    const response = await fetchTodosApiCall();
+  const response = await fetchTodosApiCall();
 
-    const fechedTodos = response?.data ?? [];
-    return (
-        <div className="flex flex-col space-y-8">
-            <h1 className={title()}>Todos</h1>
-            <TodosTables todos={fechedTodos} />
-        </div>
-    );
+  const fechedTodos = response?.data ?? [];
+  return (
+    <div className="flex flex-col space-y-8">
+      <h1 className={title()}>Todos</h1>
+      <TodosTables todos={fechedTodos} />
+    </div>
+  );
 }
