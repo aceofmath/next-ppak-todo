91e6de4241f759eaf0520d6d689e68bb4d4b838f
aceofmath <aceofmath@naver.com>
2025-04-09 23:18:58 +0900

    CSS수정정

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,8 +2,16 @@ import { title } from "@/components/primitives";
 
 export default function PricingPage() {
   return (
-    <div>
-      <h1 className={title()}>Pricing</h1>
+    // <div>
+    //   <h1 className={title()}>Pricing</h1>
+    // </div>
+
+    <div className="w-full lg:px-16 mt-12">
+      <div className="text-center">
+        <h1 className="mb-2 font-bold text-4xl">HeroUI Latest Updates</h1>
+        <h5 className="text-default-500 text-lg">All the latest news about HeroUI.</h5>
+      </div>
+      dddddd
     </div>
   );
 }
--- components/todos-table.tsx
+++ components/todos-table.tsx
@@ -15,220 +15,220 @@ import { Modal, ModalContent, useDisclosure } from "@heroui/modal";
 import { CustomModal } from "./custom-modal";
 
 export default function TodosTables({ todos }: { todos: Todo[] }) {
-    //할일 추가 가능 여부
-    const [todoAddEnable, setTodoAddEnable] = useState(false);
+  //할일 추가 가능 여부
+  const [todoAddEnable, setTodoAddEnable] = useState(false);
 
-    //입력된 할일
-    const [newTodoInput, setNewTodoInput] = useState("");
+  //입력된 할일
+  const [newTodoInput, setNewTodoInput] = useState("");
 
-    //로딩상태
-    const [isLoading, setIsLoading] = useState(false);
+  //로딩상태
+  const [isLoading, setIsLoading] = useState(false);
 
-    // 오픈 모달 상태
-    const [currentModalData, setCurrentModalData] = useState<focusedTodo>({
-        focusedTodo: null,
-        modalType: "detail" as CustomModalType,
+  // 오픈 모달 상태
+  const [currentModalData, setCurrentModalData] = useState<focusedTodo>({
+    focusedTodo: null,
+    modalType: "detail" as CustomModalType,
+  });
+
+  const router = useRouter();
+
+  const applyIsDoneUI = (isDone: boolean) => (isDone ? "line-through text-gray-900/50 dark: text-white/40" : "");
+
+  const TodoRow = (aTodo: Todo) => {
+    return (
+      <TableRow key={aTodo.id}>
+        <TableCell className={applyIsDoneUI(aTodo.is_done)}>{aTodo.id.slice(0, 4)}</TableCell>
+        <TableCell className={applyIsDoneUI(aTodo.is_done)}>{aTodo.title}</TableCell>
+        <TableCell>{aTodo.is_done ? "✅" : "❓"}</TableCell>
+        <TableCell className={applyIsDoneUI(aTodo.is_done)}>{`${aTodo.created_at}`}</TableCell>
+        <TableCell>
+          <div className="relative flex justify-center items-center gap-2">
+            <Dropdown>
+              <DropdownTrigger>
+                <Button isIconOnly size="sm" variant="light">
+                  <VerticalDotsIcon className="text-default-300" />
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                onAction={(key) => {
+                  console.log(`aTodo.id : ${aTodo.id}, key : ${key}`);
+                  setCurrentModalData({ focusedTodo: aTodo, modalType: key as CustomModalType });
+                  onOpen();
+                }}
+              >
+                <DropdownItem key="detail">상세보기</DropdownItem>
+                <DropdownItem key="update">수정</DropdownItem>
+                <DropdownItem key="delete">삭제</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        </TableCell>
+      </TableRow>
+    );
+  };
+
+  const addATodoHandler = async (title: string) => {
+    console.log("title", title);
+    if (!todoAddEnable) {
+      return;
+    }
+
+    setTodoAddEnable(false);
+    setIsLoading(true);
+    // setTimeout(() => {
+    //   console.log("....");
+    // }, 5000);
+    await new Promise((f) => setTimeout(f, 600));
+
+    console.log("addATodoHandler called");
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos`, {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+      }),
+      cache: "no-store",
     });
 
-    const router = useRouter();
+    setNewTodoInput("");
+    router.refresh();
+    setIsLoading(false);
+    notifySuccessEvent("할일 추가 되었습니다.");
+    console.log(`할일 추가 완료 : ${newTodoInput}`);
+  };
 
-    const applyIsDoneUI = (isDone: boolean) => (isDone ? "line-through text-gray-900/50 dark: text-white/40" : "");
+  const editATodoHandler = async (id: string, editedTitle: string, editedIsDone: boolean) => {
+    setIsLoading(true);
+    // setTimeout(() => {
+    //   console.log("....");
+    // }, 5000);
+    await new Promise((f) => setTimeout(f, 600));
 
-    const TodoRow = (aTodo: Todo) => {
-        return (
-            <TableRow key={aTodo.id}>
-                <TableCell className={applyIsDoneUI(aTodo.is_done)}>{aTodo.id.slice(0, 4)}</TableCell>
-                <TableCell className={applyIsDoneUI(aTodo.is_done)}>{aTodo.title}</TableCell>
-                <TableCell>{aTodo.is_done ? "✅" : "❓"}</TableCell>
-                <TableCell className={applyIsDoneUI(aTodo.is_done)}>{`${aTodo.created_at}`}</TableCell>
-                <TableCell>
-                    <div className="relative flex justify-end items-center gap-2">
-                        <Dropdown>
-                            <DropdownTrigger>
-                                <Button isIconOnly size="sm" variant="light">
-                                    <VerticalDotsIcon className="text-default-300" />
-                                </Button>
-                            </DropdownTrigger>
-                            <DropdownMenu
-                                onAction={(key) => {
-                                    console.log(`aTodo.id : ${aTodo.id}, key : ${key}`);
-                                    setCurrentModalData({ focusedTodo: aTodo, modalType: key as CustomModalType });
-                                    onOpen();
-                                }}
-                            >
-                                <DropdownItem key="detail">상세보기</DropdownItem>
-                                <DropdownItem key="update">수정</DropdownItem>
-                                <DropdownItem key="delete">삭제</DropdownItem>
-                            </DropdownMenu>
-                        </Dropdown>
-                    </div>
-                </TableCell>
-            </TableRow>
-        );
-    };
+    console.log("editATodoHandler called");
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos/${id}`, {
+      method: "POST",
+      body: JSON.stringify({
+        title: editedTitle,
+        is_done: editedIsDone,
+      }),
+      cache: "no-store",
+    });
 
-    const addATodoHandler = async (title: string) => {
-        console.log("title", title);
-        if (!todoAddEnable) {
-            return;
-        }
+    router.refresh();
+    setIsLoading(false);
+    notifySuccessEvent("할일 수정 되었습니다.");
+    console.log(`할일 수정 완료 : ${newTodoInput}`);
+  };
 
-        setTodoAddEnable(false);
-        setIsLoading(true);
-        // setTimeout(() => {
-        //   console.log("....");
-        // }, 5000);
-        await new Promise((f) => setTimeout(f, 600));
+  const deleteATodoHandler = async (id: string) => {
+    setIsLoading(true);
+    // setTimeout(() => {
+    //   console.log("....");
+    // }, 5000);
+    await new Promise((f) => setTimeout(f, 600));
 
-        console.log("addATodoHandler called");
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos`, {
-            method: "POST",
-            body: JSON.stringify({
-                title: title,
-            }),
-            cache: "no-store",
-        });
+    console.log("deleteATodoHandler called");
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos/${id}`, {
+      method: "DELETE",
+      cache: "no-store",
+    });
 
-        setNewTodoInput("");
-        router.refresh();
-        setIsLoading(false);
-        notifySuccessEvent("할일 추가 되었습니다.");
-        console.log(`할일 추가 완료 : ${newTodoInput}`);
-    };
+    router.refresh();
+    setIsLoading(false);
+    notifySuccessEvent("할일이 삭제 되었습니다.");
+    console.log(`할일 삭제 완료 : ${newTodoInput}`);
+  };
 
-    const editATodoHandler = async (id: string, editedTitle: string, editedIsDone: boolean) => {
-        setIsLoading(true);
-        // setTimeout(() => {
-        //   console.log("....");
-        // }, 5000);
-        await new Promise((f) => setTimeout(f, 600));
-
-        console.log("editATodoHandler called");
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos/${id}`, {
-            method: "POST",
-            body: JSON.stringify({
-                title: editedTitle,
-                is_done: editedIsDone,
-            }),
-            cache: "no-store",
-        });
-
-        router.refresh();
-        setIsLoading(false);
-        notifySuccessEvent("할일 수정 되었습니다.");
-        console.log(`할일 수정 완료 : ${newTodoInput}`);
-    };
-
-    const deleteATodoHandler = async (id: string) => {
-        setIsLoading(true);
-        // setTimeout(() => {
-        //   console.log("....");
-        // }, 5000);
-        await new Promise((f) => setTimeout(f, 600));
-
-        console.log("deleteATodoHandler called");
-        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/todos/${id}`, {
-            method: "DELETE",
-            cache: "no-store",
-        });
-
-        router.refresh();
-        setIsLoading(false);
-        notifySuccessEvent("할일이 삭제 되었습니다.");
-        console.log(`할일 삭제 완료 : ${newTodoInput}`);
-    };
-
-    const DisabledTodoAddButton = () => {
-        return (
-            <Popover placement="top">
-                <PopoverTrigger>
-                    <Button color="default" className="h-14">
-                        추가
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                    <div className="px-1 py-2">
-                        <div className="text-small font-bold">🦉</div>
-                        <div className="text-tiny">할일을 입력해주세요!</div>
-                    </div>
-                </PopoverContent>
-            </Popover>
-        );
-    };
-    const notifySuccessEvent = (msg: string) => toast.success(msg);
-
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
-    const ModalComponent = () => {
-        return (
-            <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange}>
-                <ModalContent>
-                    {(onClose) =>
-                        currentModalData.focusedTodo && (
-                            <CustomModal
-                                focusedTodo={currentModalData.focusedTodo}
-                                modalType={currentModalData.modalType}
-                                onClose={onClose}
-                                onEdit={async (id, title, isDone) => {
-                                    console.log(id, title, isDone);
-                                    await editATodoHandler(id, title, isDone);
-                                    onClose();
-                                }}
-                                onDelete={async (id) => {
-                                    console.log(id);
-                                    await deleteATodoHandler(id);
-                                    onClose();
-                                }}
-                            />
-                        )
-                    }
-                </ModalContent>
-            </Modal>
-        );
-    };
-
+  const DisabledTodoAddButton = () => {
     return (
-        <div className="flex flex-col space-y-2">
-            <ModalComponent />
-            <div>
-                <ToastContainer position="top-right" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick={false} rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
-            </div>
-            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                <Input
-                    label="새로운 할일"
-                    type="text"
-                    value={newTodoInput}
-                    onValueChange={(changedInput) => {
-                        setNewTodoInput(changedInput);
-                        setTodoAddEnable(changedInput.length > 0);
-                    }}
-                />
-                {todoAddEnable ? (
-                    <Button
-                        color="warning"
-                        className="h-14"
-                        onPress={async () => {
-                            await addATodoHandler(newTodoInput);
-                        }}
-                    >
-                        추가
-                    </Button>
-                ) : (
-                    DisabledTodoAddButton()
-                )}
-            </div>
-            <div className="h-2">{isLoading && <Spinner size="sm" color="warning" />}</div>
-
-            <Table aria-label="Example static collection table">
-                <TableHeader>
-                    <TableColumn>아이디</TableColumn>
-                    <TableColumn>할일내용</TableColumn>
-                    <TableColumn>완료여부</TableColumn>
-                    <TableColumn>생성일</TableColumn>
-                    <TableColumn>기능</TableColumn>
-                </TableHeader>
-                <TableBody emptyContent={"조회된 데이터가 없습니다."}>{todos && todos.map((aTodo: Todo) => TodoRow(aTodo))}</TableBody>
-            </Table>
-        </div>
+      <Popover placement="top">
+        <PopoverTrigger>
+          <Button color="default" className="h-14">
+            추가
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="px-1 py-2">
+            <div className="text-small font-bold">🦉</div>
+            <div className="text-tiny">할일을 입력해주세요!</div>
+          </div>
+        </PopoverContent>
+      </Popover>
     );
+  };
+  const notifySuccessEvent = (msg: string) => toast.success(msg);
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const ModalComponent = () => {
+    return (
+      <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) =>
+            currentModalData.focusedTodo && (
+              <CustomModal
+                focusedTodo={currentModalData.focusedTodo}
+                modalType={currentModalData.modalType}
+                onClose={onClose}
+                onEdit={async (id, title, isDone) => {
+                  console.log(id, title, isDone);
+                  await editATodoHandler(id, title, isDone);
+                  onClose();
+                }}
+                onDelete={async (id) => {
+                  console.log(id);
+                  await deleteATodoHandler(id);
+                  onClose();
+                }}
+              />
+            )
+          }
+        </ModalContent>
+      </Modal>
+    );
+  };
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <ModalComponent />
+      <div>
+        <ToastContainer position="top-right" autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick={false} rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
+      </div>
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <Input
+          label="새로운 할일"
+          type="text"
+          value={newTodoInput}
+          onValueChange={(changedInput) => {
+            setNewTodoInput(changedInput);
+            setTodoAddEnable(changedInput.length > 0);
+          }}
+        />
+        {todoAddEnable ? (
+          <Button
+            color="warning"
+            className="h-14"
+            onPress={async () => {
+              await addATodoHandler(newTodoInput);
+            }}
+          >
+            추가
+          </Button>
+        ) : (
+          DisabledTodoAddButton()
+        )}
+      </div>
+      <div className="h-2">{isLoading && <Spinner size="sm" color="warning" />}</div>
+
+      <Table aria-label="Example static collection table">
+        <TableHeader>
+          <TableColumn>아이디</TableColumn>
+          <TableColumn>할일내용</TableColumn>
+          <TableColumn>완료여부</TableColumn>
+          <TableColumn>생성일</TableColumn>
+          <TableColumn>기능</TableColumn>
+        </TableHeader>
+        <TableBody emptyContent={"조회된 데이터가 없습니다."}>{todos && todos.map((aTodo: Todo) => TodoRow(aTodo))}</TableBody>
+      </Table>
+    </div>
+  );
 }
